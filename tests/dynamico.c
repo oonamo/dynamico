78067154393e6c6335e1067af05dd1017a2526f0
oonamo <onam75hernandez@gmail.com>
2024-10-18 21:16:15 -0700

    formatting

--- tests/dynamico.c
+++ tests/dynamico.c
@@ -10,7 +10,7 @@
     fprintf(stderr,                                                            \
             DYNAMICO_TERMINAL_YELLOW                                           \
             "TEST " DYNAMICO_TERMINAL_GREEN "%s" DYNAMICO_TERMINAL_RESET       \
-            " (" DYNAMICO_TERMINAL_BLUE "%s" DYNAMICO_TERMINAL_RESET       \
+            " (" DYNAMICO_TERMINAL_BLUE "%s" DYNAMICO_TERMINAL_RESET           \
             "): %s\n",                                                         \
             TEST_NAME, __func__, __FILE__)
 
@@ -18,7 +18,7 @@
     fprintf(stderr,                                                            \
             DYNAMICO_TERMINAL_YELLOW                                           \
             "TEST " DYNAMICO_TERMINAL_GREEN "%s" DYNAMICO_TERMINAL_RESET       \
-            " (" DYNAMICO_TERMINAL_BLUE "%s" DYNAMICO_TERMINAL_RESET       \
+            " (" DYNAMICO_TERMINAL_BLUE "%s" DYNAMICO_TERMINAL_RESET           \
             "): %s " DYNAMICO_TERMINAL_GREEN "PASSED!" DYNAMICO_TERMINAL_RESET \
             "\n",                                                              \
             TEST_NAME, __func__, __FILE__)
@@ -27,21 +27,21 @@
     fprintf(stderr,                                                            \
             DYNAMICO_TERMINAL_YELLOW                                           \
             "TEST " DYNAMICO_TERMINAL_GREEN "%s" DYNAMICO_TERMINAL_RESET       \
-            " (" DYNAMICO_TERMINAL_BLUE "%s" DYNAMICO_TERMINAL_RESET       \
+            " (" DYNAMICO_TERMINAL_BLUE "%s" DYNAMICO_TERMINAL_RESET           \
             "): %s " DYNAMICO_TERMINAL_RED "FAILED!" DYNAMICO_TERMINAL_RESET   \
             "\n",                                                              \
             TEST_NAME, __func__, __FILE__)
 
-#define DYNAMICO_EVALUATE_TEST()                                           \
+#define DYNAMICO_EVALUATE_TEST()                                               \
     do                                                                         \
     {                                                                          \
         if (result == DYNAMICO_FAIL)                                           \
         {                                                                      \
-            DYNAMICO_TEST_FAIL();                                          \
+            DYNAMICO_TEST_FAIL();                                              \
         }                                                                      \
         else                                                                   \
         {                                                                      \
-            DYNAMICO_TEST_PASS();                                          \
+            DYNAMICO_TEST_PASS();                                              \
         }                                                                      \
     } while (0)
 
